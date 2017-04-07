1239a5ce8292395be0d78791bdacc51db294dc39
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-04-07 16:04:55 -0400

    update blog

---
+++ src/resources/alexalogin.js
@@ -0,0 +1,151 @@
+function getWindowLocationSearch(win) {
+    'use strict';
+
+    var search = (win || window).location.search;
+
+    if (!search) {
+
+        var index = window.location.href.indexOf('?');
+        if (index != -1) {
+            search = window.location.href.substring(index);
+        }
+    }
+
+    return search || '';
+}
+
+function getParameterByName(name, url) {
+    'use strict';
+
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS, "i");
+
+    var results = regex.exec(url || getWindowLocationSearch());
+    if (results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function setParam(url, name, value) {
+    
+    if (!value) {
+        return url;
+    }
+
+    if (url.indexOf('?') === -1) {
+
+        url += '?';
+    } else {
+        url += '&';
+    }
+
+    url += name + '=' + value;
+    return url;
+}
+
+function getAlexaAccessToken(values) {
+
+    return values.join('___');
+}
+
+jQuery(document).ready(function ($) {
+
+    var loginResult;
+
+    //get current logged in user
+    $.ajaxSetup({
+        headers: {
+            'X-Connect-Token': 'CONNECT-GETUSER'
+        }
+    });
+
+    function onLoginFail() {
+        $('#msg').html("Invalid User Name or Password.  Do you need to register?").show();
+    }
+
+    function showServers() {
+        $('#confirmForm').hide();
+        $('#selectServerForm').show();
+
+        getConnectServers().then(function (servers) {
+
+            document.querySelector('#selectServer').innerHTML = servers.map(function (s) {
+
+                return '<option value="' + s.SystemId + '">' + s.Name + '</option>';
+
+            }).join('');
+        });
+    }
+
+    function getConnectServers() {
+
+        console.log('Begin getConnectServers');
+
+        var url = "https://connect.emby.media/service/servers?userId=" + loginResult.User.Id;
+
+        return $.ajax({
+            type: "GET",
+            url: url,
+            dataType: "json",
+            headers: {
+                "X-Application": "EmbyWebSite/1.0.0",
+                "X-Connect-UserToken": loginResult.AccessToken
+            }
+
+        });
+    }
+
+    $('#confirmForm').on('submit', function () {
+
+        $('#msg').hide();
+
+        var username = $('#username').val();
+
+        $.ajax({
+            type: "POST",
+            url: "https://connect.emby.media/service/user/authenticate",
+            data: {
+                nameOrEmail: username,
+                rawpw: $('#pw').val()
+            }
+        }).done(function (json) {
+
+            var result = $.parseJSON(json);
+            console.log(result);
+            var user = result.User;
+            if (user && user.Id > 0) {
+                loginResult = result;
+                showServers();
+            } else {
+                onLoginFail();
+            }
+
+        }).fail(onLoginFail);
+
+        return false;
+    });
+
+    $('#selectServerForm').on('submit', function (e) {
+
+        var form = this;
+        var serverId = form.querySelector('#selectServer').value;
+
+        // redirect to redirect_uri?state=state&access_token=access_token&token_type=Bearer
+        var values = [];
+        values.push(serverId);
+
+        var alexaAccessToken = getAlexaAccessToken(values);
+
+        var redirectUri = getParameterByName('redirect_uri');
+
+        redirectUri = setParam(redirectUri, 'token_type', 'Bearer');
+        redirectUri = setParam(redirectUri, 'state', getParameterByName('state'));
+        redirectUri = setParam(redirectUri, 'access_token', alexaAccessToken);
+
+        window.location.href = redirectUri;
+
+        return false;
+    });
+});
