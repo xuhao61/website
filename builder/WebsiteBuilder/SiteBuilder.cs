f44efc72c72ec39580f57cfceccd609bc4289a9b
Luke Pulverenti <luke.pulverenti@gmail.com>
2019-10-22 13:10:35 -0400

    update samsung

--- builder/WebsiteBuilder/SiteBuilder.cs
+++ builder/WebsiteBuilder/SiteBuilder.cs
@@ -204,7 +204,7 @@ namespace WebsiteBuilder
             script += "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-88826918-1', 'auto');ga('send', 'pageview');";
             script += "</script>";
 
-            return html.Replace("</body>", script + "</body");
+            return html.Replace("</body>", script + "</body>");
         }
 
         private string GetImageUrlFromSubTree(XmlReader reader)
